f68753b168369d655d64a2c9e03d9115c629ace9
praneethvalameti93 <praneethvalameti93@gmail.com>
2019-01-26 22:53:39 +0530

    added urls in sw.js to store in cache

--- sw.js
+++ sw.js
@@ -6,24 +6,24 @@ this.addEventListener('install', event => {
     caches.open(CACHE_NAME).then(cache => {
       cache.addAll(
         [
-          // '/index.html'
-          // '/css/styles.css',
-          // '/restaurant.html',
-          // '/css/restaurant.css',
-          // '/data/restaurant.json',
-          // '/js/main.js',
-          // '/js/dbhelper.js',
-          // '/js/restaurant_info.js',
-          // '/img/1.jpg',
-          // '/img/2.jpg',
-          // '/img/3.jpg',
-          // '/img/4.jpg',
-          // '/img/5.jpg',
-          // '/img/6.jpg',
-          // '/img/7.jpg',
-          // '/img/8.jpg',
-          // '/img/9.jpg',
-          // '/img/10.jpg'
+          '/index.html',
+          '/css/styles.css',
+          '/restaurant.html',
+          '/css/restaurant.css',
+          '/data/restaurant.json',
+          '/js/main.js',
+          '/js/dbhelper.js',
+          '/js/restaurant_info.js',
+          '/img/1.jpg',
+          '/img/2.jpg',
+          '/img/3.jpg',
+          '/img/4.jpg',
+          '/img/5.jpg',
+          '/img/6.jpg',
+          '/img/7.jpg',
+          '/img/8.jpg',
+          '/img/9.jpg',
+          '/img/10.jpg'
         ]
       )
     })
