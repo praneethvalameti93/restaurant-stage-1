4a9e6ed419363ab144a6ea013389074ef0769369
praneethvalameti93 <praneethvalameti93@gmail.com>
2019-01-26 23:22:36 +0530

    modified in sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 // the fallowing variable is used to store the name of the version
-var CACHE_NAME = 'vr1';
+var CACHE_DB_NAME = 'vr1';
 this.addEventListener('install', event => {
   // waituntil is used to wait untill files store in the cache
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+    caches.open(CACHE_DB_NAME).then(cache => {
       cache.addAll(
         [
           // '/index.html',
@@ -33,7 +33,7 @@ this.addEventListener('install', event => {
 // fetch is used to data from the cache and also clone the data and store in cache
 this.addEventListener('fetch', event => {
   event.respondWith(
-    caches.open(CACHE_NAME).then(cache => {
+    caches.open(CACHE_DB_NAME).then(cache => {
       return cache.match(event.request).then(res => {
         return res || fetch(event.request).then(res => {
           cache.put(event.request, res.clone());
